163ea985f3bd7e6d448e3545798f92ab99dbbc0c
Arlo White <arlo.white@gmail.com>
2015-09-16 15:26:05 -0700

    Use moment for date parsing. Hack Datepicker to update the way I want.

--- www/js/app.js
+++ www/js/app.js
@@ -4,6 +4,57 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
+
+var momentDateParseFormats = ['MMM-DD-YYYY', 'dddd, MMMM Do'];
+angular.module('material.components.datepicker')
+/**
+ * Override mdDatepicker to use moment's isValid so that
+ * bad dates such as "Oct 44" are marked invalid.
+ *
+ */
+  .run(function(mdDatepickerDirective) {
+    var ctrlProto = mdDatepickerDirective[0].controller.prototype;
+    var INVALID_CLASS = 'md-datepicker-invalid';
+    ctrlProto.handleInputEvent = function() {
+      var inputString = this.inputElement.value;
+      var momentDate = moment(inputString, momentDateParseFormats);
+      var parsedDate = momentDate.toDate();
+      this.dateUtil.setDateTimeToMidnight(parsedDate);
+
+      if (inputString === '') {
+        this.ngModelCtrl.$setViewValue(null);
+        this.date = null;
+        this.inputContainer.classList.remove(INVALID_CLASS);
+      } else if (momentDate.isValid() &&
+        this.dateUtil.isDateWithinRange(parsedDate, this.minDate, this.maxDate)) {
+        this.ngModelCtrl.$setViewValue(parsedDate);
+        this.date = parsedDate;
+        this.inputContainer.classList.remove(INVALID_CLASS);
+
+        this._needsRenderOnBlur = this.isFocused;
+        if(!this.isFocused){
+          // Debounced after blur, Render now
+          this.inputElement.value = this.dateLocale.formatDate(this.date);
+          this.resizeInputElement();
+        }
+      } else {
+        // If there's an input string, it's an invalid date.
+        this.inputContainer.classList.toggle(INVALID_CLASS, inputString);
+        this._needsRenderOnBlur = false;
+      }
+    };
+    ctrlProto.setFocused = function(isFocused) {
+      this.isFocused = isFocused;
+      if(!isFocused && this._needsRenderOnBlur){
+        // Blur
+        this.inputElement.value = this.dateLocale.formatDate(this.date);
+        this.resizeInputElement();
+        this._needsRenderOnBlur = false;
+      }
+    };
+  });
+
+
 angular.module('sportSocial', ['ionic','ionic.service.core','ionic.service.deploy', 'ngCordova', 'ngMaterial',
   'ngProgress',
   'sportSocial.controllers', 'sportSocial.services'])
@@ -47,7 +98,8 @@ angular.module('sportSocial', ['ionic','ionic.service.core','ionic.service.deplo
     });
   })
 
-  .config(function($stateProvider, $urlRouterProvider, $ionicConfigProvider, $mdGestureProvider) {
+  .config(function($stateProvider, $urlRouterProvider, $ionicConfigProvider, $mdGestureProvider,
+                   $mdDateLocaleProvider) {
     // Native scrolling recommended by:
     // http://julienrenaux.fr/2015/08/24/ultimate-angularjs-and-ionic-performance-cheat-sheet/
     // http://blog.ionic.io/native-scrolling-in-ionic-a-tale-in-rhyme/
@@ -63,6 +115,23 @@ angular.module('sportSocial', ['ionic','ionic.service.core','ionic.service.deplo
     // FIXME need skipClickHijack?
     // http://forum.ionicframework.com/t/is-there-a-tutorial-for-using-ionic-and-angular-material/14662/23
     $mdGestureProvider.skipClickHijack();
+
+    $mdDateLocaleProvider.parseDate = function(dateString) {
+      return moment(dateString, momentDateParseFormats).toDate();
+    };
+
+    // May not be used at all since handleInputEvent overridden above.
+    $mdDateLocaleProvider.isDateComplete = function () {
+      // moment handles parsing and validity is checked anyway
+      return true;
+    };
+
+    $mdDateLocaleProvider.formatDate = function(date) {
+      if(!date){
+        return '';
+      }
+      return moment(date).format('dddd, MMMM Do');
+    };
 
     $stateProvider
 
