5c6c2fcbd3941f7348bd70d873bb671ed22a5605
Arlo White <arlo.white@gmail.com>
2015-10-23 16:15:28 -0700

    Fix SASS compilation of Ionic and Angular Material. Minimize loaded Ionic CSS.

---
+++ gulp/material-css.js
@@ -0,0 +1,30 @@
+'use strict';
+
+var path = require('path');
+var gulp = require('gulp');
+var conf = require('./conf');
+
+var browserSync = require('browser-sync');
+
+var $ = require('gulp-load-plugins')();
+
+var wiredep = require('wiredep').stream;
+var _ = require('lodash');
+
+gulp.task('material-css', function () {
+  // https://github.com/sass/node-sass#options
+  var sassOptions = {
+    style: 'expanded'
+  };
+
+  return gulp.src([
+    path.join(conf.paths.src, '/angular-material/angular-material.scss')
+  ])
+    .pipe(wiredep(_.extend({}, conf.wiredep)))
+    .pipe($.sourcemaps.init())
+    .pipe($.sass(sassOptions)).on('error', conf.errorHandler('Sass'))
+    .pipe($.autoprefixer()).on('error', conf.errorHandler('Autoprefixer'))
+    .pipe($.sourcemaps.write())
+    .pipe(gulp.dest(path.join(conf.paths.tmp, '/serve/app/')))
+    .pipe(browserSync.reload({ stream: true }));
+});
--- gulp/styles.js
+++ gulp/styles.js
@@ -2,7 +2,6 @@
 
 var path = require('path');
 var gulp = require('gulp');
-var debug = require('gulp-debug');
 var conf = require('./conf');
 
 var browserSync = require('browser-sync');
@@ -20,7 +19,8 @@ gulp.task('styles', function () {
 
   var injectFiles = gulp.src([
     path.join(conf.paths.src, '/app/**/*.scss'),
-    path.join('!' + conf.paths.src, '/app/index.scss')
+    path.join('!' + conf.paths.src, '/app/index.scss'),
+    path.join('!' + conf.paths.src, '/app/**/_*.scss')
   ], { read: false });
 
   var injectOptions = {
@@ -33,12 +33,69 @@ gulp.task('styles', function () {
     addRootSlash: false
   };
 
+  // The Ionic SCSS files to include
+  var ionicScssFilenames = [
+    // Ionicons
+    "ionicons/ionicons.scss",
+
+    // Variables
+    "mixins",
+    "variables",
+
+    // Base
+    "reset",
+    "scaffolding",
+    "type",
+
+    // Components
+    //"action-sheet",
+    "backdrop",
+    "bar",
+    "tabs",
+    //"menu",
+    //"modal",
+    //"popover",
+    //"popup",
+    "loading",
+    //"items",
+    //"list",
+    "badge",
+    //"slide-box",
+    "refresher",
+    //"spinner",
+
+    // Forms
+    //"form",
+    //"checkbox",
+    //"toggle",
+    //"radio",
+    //"range",
+    //"select",
+    //"progress",
+
+    // Buttons
+    //"button",
+    //"button-bar",
+
+    // Util
+    //"grid",
+    "util",
+    "platform",
+
+    // Animations
+    "animations",
+    "transitions"];
+
+  var paths = ionicScssFilenames.map(function (name) {
+    return '@import "../../bower_components/ionic/scss/' + name + '";';
+  });
+  var ionicImports = paths.join("\n");
 
   return gulp.src([
     path.join(conf.paths.src, '/app/index.scss')
   ])
     .pipe($.inject(injectFiles, injectOptions))
-    .pipe(debug())
+    .pipe($.replace('// IONIC_IMPORTS', ionicImports))
     .pipe(wiredep(_.extend({}, conf.wiredep)))
     .pipe($.sourcemaps.init())
     .pipe($.sass(sassOptions)).on('error', conf.errorHandler('Sass'))
